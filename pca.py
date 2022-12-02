18a32a0b28173519a9d61f0cc21a4df381c594cc
Joeldev89 <joelorellana989@gmail.com>
2022-12-02 13:17:20 -0600

    Commit inicial

---
+++ pca.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import sklearn
+import matplotlib.pyplot as plt
+
+from sklearn.decomposition import PCA
+from sklearn.decomposition import IncrementalPCA
+
+# La regresión logística sirve para clasificar
+
+from sklearn.linear_model import LogisticRrgression
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+if __name__ == '__main__':
+    
+
+
